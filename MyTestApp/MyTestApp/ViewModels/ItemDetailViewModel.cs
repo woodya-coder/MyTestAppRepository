1ff99455b3e596125cfc85670a34a6682a44d979
Elwood <Elwood@DESKTOP-8OQD2DU>
2020-02-26 16:54:50 -0800

    Initially generated project files

---
+++ MyTestApp/MyTestApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyTestApp.Models;
+
+namespace MyTestApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
